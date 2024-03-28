4e767d40ae8bfbe61b4591ced1bd527903cb0204
Steve Roques <steve.roques@gmail.com>
2024-03-28 10:29:03 +0100

    using em_filter

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,6 @@
 use actix_web::{post, HttpResponse, Responder, App, HttpServer};
-use reqwest;
 use rand::Rng;
-use serde::{Serialize, Deserialize};
 use embryo::{Embryo, EmPair, EmbryoList};
-
-static PORT: i32 = 8081;
-static REGISTRY_URL: &str = "http://localhost:8080";
-
-#[derive(Serialize, Deserialize)]
-struct FilterInfo {
-    url: String,
-}
 
 #[post("/query")]
 async fn query_handler(body: String) -> impl Responder {
@@ -35,7 +25,7 @@ fn generate_embryo_list(json_string: String) -> Vec<Embryo> {
                 properties: vec![
                     EmPair {
                         name: "url".to_string(),
-                        value: format!("http://example.com:{}/{}", PORT, random_number),
+                        value: format!("http://example/{}", random_number),
                     },
                     EmPair {
                         name: "resume".to_string(),
@@ -52,33 +42,18 @@ fn generate_embryo_list(json_string: String) -> Vec<Embryo> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Register filter on startup
-    let filter_url = format!("http://localhost:{}/query", PORT);
-    register_filter(&filter_url).await;
-    println!("Filter registered: {}", filter_url);
-
-    // Start Actix web server
-    HttpServer::new(|| App::new().service(query_handler))
-        .bind(format!("127.0.0.1:{}", PORT))?
-        .run()
-        .await?;
-
+    match em_filter::find_port().await {
+        Some(port) => {
+            let filter_url = format!("http://localhost:{}/query", port);
+            println!("Filter registrer: {}", filter_url);
+            em_filter::register_filter(&filter_url).await;
+            HttpServer::new(|| App::new().service(query_handler))
+                .bind(format!("127.0.0.1:{}", port))?.run().await?;
+        },
+        None => {
+            println!("Can't start");
+        },
+    }
     Ok(())
-}
-
-async fn register_filter(url: &str) {
-    let filter_info = FilterInfo { url: url.to_string() };
-    let register_url = format!("{}/register", REGISTRY_URL);
-
-    match reqwest::Client::new()
-        .post(&register_url)
-        .header("Content-Type", "application/json")
-        .body(serde_json::to_string(&filter_info).expect("Error serializing JSON"))
-        .send()
-        .await
-        {
-            Ok(_) => println!("Filter registered with the central registry."),
-            Err(e) => eprintln!("Failed to register filter: {:?}", e),
-        }
 }
 
